229989269f36a7f930e43c9020734753953ee91c
320187 <320187240@qq.com>
2025-02-28 00:21:28 +0800

    init

--- ruoyi-ui/src/views/login.vue
+++ ruoyi-ui/src/views/login.vue
@@ -72,8 +72,8 @@ export default {
     return {
       codeUrl: "",
       loginForm: {
-        username: "tony",
-        password: "123456",
+        username: "admin",
+        password: "admin123",
         rememberMe: false,
         code: "",
         uuid: ""
